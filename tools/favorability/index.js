400f2684de4dec23bc30750c69ae48eb2401d54d
Yuhanawa <3261817910@qq.com>
2022-08-10 22:43:48 +0800

    Site updated: 2022-08-10 22:43:48

---
+++ tools/favorability/index.js
@@ -0,0 +1,45 @@
+let OriginalScore = 0;
+let master = "🎂蛋糕🎂"
+let pos = 0;
+questions=[ // viewpoints
+    "你永远喜欢%",
+    "你对%一心一意",
+    "你对%绝对忠诚",
+    "你认为%一定是个好人",
+    "你永远单推%",
+    "你永远支持%",
+    "你愿意做%的狗",
+    "你愿意拯救%",
+    "你原为%献上生命",
+    "你原为%线上父母",
+    "你愿舔%的脚丫",
+    "你敢于在公共场所抢夺%",
+]
+questions.forEach((s,i)=>{questions[i] = s.replace("%", master);});
+
+
+function next_question(i) {
+    OriginalScore += i;
+    pos++;
+
+    if (pos >= questions.length) {
+        document.getElementById("question-text").innerHTML = "Your score is " + get_score();
+        document.getElementById("question-number").innerHTML = "";
+        document.getElementById("question-button").innerHTML = "";
+        document.getElementById("question-button").style.display = "none";
+    } else {
+        document.getElementById("question-text").innerHTML = questions[pos];
+        document.getElementById("question-number").innerHTML = "Question " + (pos+1) + " of " + questions.length;
+    }
+}
+
+function get_score() {
+    return OriginalScore/ (questions.length * 10) * 100 ;
+}
+
+function prev_question() {
+    document.getElementById("question-text").innerHTML = "！！！ 不合格 ！！！";
+    document.getElementById("question-number").innerHTML = "";
+    document.getElementById("question-button").innerHTML = "";
+    document.getElementById("question-button").style.display = "none";
+}
