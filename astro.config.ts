36eb239204ffe1a9805a1c847c750757a89f906d
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2025-11-18 23:13:33 +0800

    style: format

--- astro.config.ts
+++ astro.config.ts
@@ -3,46 +3,46 @@ import charm from "astro-charm";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  site: "https://yuhanawa.github.io/",
+	prefetch: true,
+	site: "https://yuhanawa.github.io/",
 
-  integrations: [
-    charm({
-      config: {
-        lang: "zh",
-        title: "Yuhanawa's Blog",
-        description: "Yuhanawa's Blog",
-        author: "Yuhanawa",
-        side: {
-          title: "Yuhanawa",
-          sub: "Hello,",
-          bio: " I'm here",
-          navHome: {
-            title: "Home",
-          },
-          footer: [
-            {
-              title: "Twitter",
-              link: "https://x.com/yuhanawa",
-              icon: "simple-icons:twitter",
-            },
-            {
-              title: "GitHub",
-              link: "https://github.com/yuhanawa",
-              icon: "simple-icons:github",
-            },
-          ],
-        },
-      },
-      pages: {},
-      overrides: {
-        components: {
-          // you can add custom script by overriding CustomScriptComponent,
-          // it will be added to the end of `<head>`.
-          // example:
-          // CustomScriptComponent: "./src/components/CustomScriptComponent.astro",
-        },
-      },
-    }),
-  ],
+	integrations: [
+		charm({
+			config: {
+				lang: "zh",
+				title: "Yuhanawa's Blog",
+				description: "Yuhanawa's Blog",
+				author: "Yuhanawa",
+				side: {
+					title: "Yuhanawa",
+					sub: "Hello,",
+					bio: " I'm here",
+					navHome: {
+						title: "Home",
+					},
+					footer: [
+						{
+							title: "Twitter",
+							link: "https://x.com/yuhanawa",
+							icon: "simple-icons:twitter",
+						},
+						{
+							title: "GitHub",
+							link: "https://github.com/yuhanawa",
+							icon: "simple-icons:github",
+						},
+					],
+				},
+			},
+			pages: {},
+			overrides: {
+				components: {
+					// you can add custom script by overriding CustomScriptComponent,
+					// it will be added to the end of `<head>`.
+					// example:
+					// CustomScriptComponent: "./src/components/CustomScriptComponent.astro",
+				},
+			},
+		}),
+	],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
 import { collections as charmCollections } from "astro-charm/content";
 export const collections = {
-  ...charmCollections,
+	...charmCollections,
 };
